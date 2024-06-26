464f5601a85c51ac4b71c3c1f5c04bfbe7ddd737
nalule-grace <g.nalule@alustudent.com>
2024-06-26 22:19:29 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+Develop a stopwatch timer with start, pause, resume, reset, and stop buttons.
+The time format should be 00:00:00 for (hours:mins:secs)
+The start button should start the watch from the secs and adds up to minutes and hours
+The pause button should pause the stopwatch and prints its current value to the console or display the current value
+The resume button should continue the stopwatch from when it was paused
+The reset button should set the watch back to 00:00:00
+The stop button should stop the watch and display the last read value
--- StopwatchTimer.Tests/obj/Debug/net8.0-windows/StopwatchTimer.Tests.AssemblyInfo.cs
+++ StopwatchTimer.Tests/obj/Debug/net8.0-windows/StopwatchTimer.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StopwatchTimer.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+023fd89ba257eca998d553c6e2f579145fe088dd")]
 [assembly: System.Reflection.AssemblyProductAttribute("StopwatchTimer.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StopwatchTimer.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
